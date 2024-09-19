cea60217578944eb4497adc97014adaf79f03c4b
Jean Justen <jean.cjm@hotmail.com>
2024-09-19 20:01:20 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Parallax Effect
+let listBg = document.querySelectorAll(".bg");
+let titleBanner = document.querySelector(".motto");
+
+window.addEventListener("scroll", (event) => {
+    let top = this.scrollY;
+    // Index is the order of class (bg 0,1,2...8). When scrolling, the class bg scrolls down.
+    // The bigger the index, the faster the movement
+    listBg.forEach((bg, index) => {
+        if (index !=0 && index != 10){
+            bg.style.transform = `translateY(${top*index/2}px)`;
+        } else if (index ==1) {
+            bg.style.transform = `translateY(${top/3}px)`;
+        }
+    })
+    titleBanner.style.transform = `translateY(${top*4/2}px)`;
+});
